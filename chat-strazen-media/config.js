86fe8c014efa8da0b0ae8ac3221235d7c23a5e3f
ivan2330 <ivankozhevnyk@gmail.com>
2025-07-19 03:49:07 +0300

    /

--- chat-strazen-media/config.js
+++ chat-strazen-media/config.js
@@ -1,2 +1,2 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://starzen-psychics.com/api';
-export const WS_URL = import.meta.env.VITE_WS_URL || 'wss://starzen-psychics.com';
+export const API_URL = import.meta.env.VITE_API_URL || 'http://starzen-psychics.com/api';
+export const WS_URL = import.meta.env.VITE_WS_URL || 'ws://starzen-psychics.com';
